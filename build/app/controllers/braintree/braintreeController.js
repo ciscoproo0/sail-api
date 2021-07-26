6cb34da7ef258b22ae0f41812103f0fc3ff8e1ed
Francisco Pereira da Silva <ciscoproo0@gmail.com>
2021-07-26 14:10:13 -0300

    Urgent merchant test

--- build/app/controllers/braintree/braintreeController.js
+++ build/app/controllers/braintree/braintreeController.js
@@ -35,6 +35,7 @@ class Braintree {
 
     const response = await gateway.transaction.sale({
       amount,
+      installments: {count: 4},
       paymentMethodNonce: nonce || null,
       deviceData: dataInfo || null,
       customerId: customerId || null,
